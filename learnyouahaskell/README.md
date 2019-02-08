8958b69aea170f546801ecd0859804893fa18f46
Jimmy Cheong <jimmycheong7@gmail.com>
2019-02-08 18:53:03 +0000

    Completed getting started

---
+++ learnyouahaskell/README.md
@@ -0,0 +1,10 @@
+# Learning Haskell from learnyouahaskell
+
+Steps to run: 
+
+1. Download GHC
+2. run `ghci` on command line in the correct directory
+3. run `:set prompt "ghci >"` to bring up the ghci prompt
+4. :l <filename>
+
+    
---
+++ learnyouahaskell/baby.hs
@@ -0,0 +1,24 @@
+doubleMe x = x + x 
+
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber x = if x > 100 then x else x * 2
+
+removeNonUppercase st = 
+    [   
+        c | 
+        c <- st, 
+        c `elem` ['A'..'Z']
+    ]   
+
+-- Triange problem
+
+triangles = [ (a,b,c) | c <- [1..10], b <- [1..10], a <- [1..10]]
+
+-- Right angle triangles
+
+rightTriangles = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2] 
+
+
+-- Right angle triangles where the perimeter is 24 
+rightTriangles' = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a+b+c == 24]  
